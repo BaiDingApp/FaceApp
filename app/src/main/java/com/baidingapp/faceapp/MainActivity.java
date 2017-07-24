074709f60d2c1c3b1084f5e47a800ab33774d907
BaiDingApp <30397356+BaiDingApp@users.noreply.github.com>
2017-07-24 20:30:49 +0800

    2017/07/24 Create the FaceApp and make it interface with LeanCloud

---
+++ app/src/main/java/com/baidingapp/faceapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.baidingapp.faceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.SaveCallback;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 测试 SDK 是否正常工作的代码
+        AVObject testObject = new AVObject("TestObject");
+        testObject.put("words","Hello World!");
+        testObject.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(AVException e) {
+                if(e == null){
+                    Log.d("saved","success!");
+                }
+            }
+        });
+    }
+}
