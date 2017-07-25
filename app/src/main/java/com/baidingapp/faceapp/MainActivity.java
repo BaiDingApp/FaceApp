20d106f2810566d0d75a236b118036cc8427071b
BaiDingApp <30397356+BaiDingApp@users.noreply.github.com>
2017-07-25 17:25:01 +0800

    2017/07/25 The Bottom Navigation has completed, and the problem of screen rotation has solved.

--- app/src/main/java/com/baidingapp/faceapp/MainActivity.java
+++ app/src/main/java/com/baidingapp/faceapp/MainActivity.java
@@ -12,24 +12,33 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String KEY_INDEX = "page_index";
+    private int mPageIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            mPageIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
         setupNavigationView();
     }
 
     private void setupNavigationView() {
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        if (bottomNavigationView !=null) {
+
+        BottomNavigationView mBottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        if (mBottomNavigationView !=null) {
 
             // Select first menu item by default and show Fragment accordingly
-            Menu menu = bottomNavigationView.getMenu();
-            selectFragment(menu.getItem(0));
+            Menu menu = mBottomNavigationView.getMenu();
+            selectFragment(menu.getItem(mPageIndex));
 
             // Set action to perform when any menu-item is selected.
-            bottomNavigationView.setOnNavigationItemSelectedListener(
+            mBottomNavigationView.setOnNavigationItemSelectedListener(
                     new BottomNavigationView.OnNavigationItemSelectedListener() {
                         @Override
                         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -46,22 +55,27 @@ public class MainActivity extends AppCompatActivity {
      */
     protected void selectFragment(MenuItem item) {
 
+        // Make the corresponding Menu Item Highlighted
         item.setChecked(true);
 
         switch (item.getItemId()) {
             case R.id.action_face:
+                mPageIndex = 0;
                 // Action to perform when Face Menu item is selected
                 pushFragment(new FacePageFragment());
                 break;
             case R.id.action_resume:
+                mPageIndex = 1;
                 // Action to perform when Resume Menu item is selected
                 pushFragment(new ResumePageFragment());
                 break;
             case R.id.action_game:
+                mPageIndex = 2;
                 // Action to perform when Game Menu item is selected
                 pushFragment(new GamePageFragment());
                 break;
             case R.id.action_me:
+                mPageIndex = 3;
                 // Action to perform when Me Menu item is selected
                 pushFragment(new MePageFragment());
                 break;
@@ -73,7 +87,6 @@ public class MainActivity extends AppCompatActivity {
      * Method to push any fragment into given id.
      * @param fragment An instance of Fragment to show into the given id.
      */
-
     protected void pushFragment(Fragment fragment) {
         if (fragment == null)
             return;
@@ -82,10 +95,16 @@ public class MainActivity extends AppCompatActivity {
         if (fragmentManager != null) {
             FragmentTransaction ft = fragmentManager.beginTransaction();
             if (ft != null) {
-                ft.replace(R.id.rootLayout, fragment);
+                ft.replace(R.id.fragment_container, fragment);
                 ft.commit();
             }
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(KEY_INDEX, mPageIndex);
     }
 }
 
--- app/src/main/java/com/baidingapp/faceapp/ResumePageFragment.java
+++ app/src/main/java/com/baidingapp/faceapp/ResumePageFragment.java
@@ -9,6 +9,11 @@ import android.view.ViewGroup;
 
 public class ResumePageFragment extends Fragment {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
