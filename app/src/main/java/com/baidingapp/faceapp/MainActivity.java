9bd081aa172cf0a6c1d57089b456cde6077d5f55
BaiDingApp <30397356+BaiDingApp@users.noreply.github.com>
2017-07-25 13:43:20 +0800

    2017/07/24 Create the FaceApp and make it interface with LeanCloud

--- app/src/main/java/com/baidingapp/faceapp/MainActivity.java
+++ app/src/main/java/com/baidingapp/faceapp/MainActivity.java
@@ -1,12 +1,14 @@
 package com.baidingapp.faceapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.avos.avoscloud.AVException;
-import com.avos.avoscloud.AVObject;
-import com.avos.avoscloud.SaveCallback;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,16 +17,99 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 测试 SDK 是否正常工作的代码
-        AVObject testObject = new AVObject("TestObject");
-        testObject.put("words","Hello World!");
-        testObject.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(AVException e) {
-                if(e == null){
-                    Log.d("saved","success!");
-                }
+        setupNavigationView();
+    }
+
+    private void setupNavigationView() {
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        if (bottomNavigationView !=null) {
+
+            // Select first menu item by default and show Fragment accordingly
+            Menu menu = bottomNavigationView.getMenu();
+            selectFragment(menu.getItem(0));
+
+            // Set action to perform when any menu-item is selected.
+            bottomNavigationView.setOnNavigationItemSelectedListener(
+                    new BottomNavigationView.OnNavigationItemSelectedListener() {
+                        @Override
+                        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                            selectFragment(item);
+                            return false;
+                        }
+                    });
+        }
+    }
+
+    /**
+     * Perform action when any item is selected.
+     * @param item Item that is selected.
+     */
+    protected void selectFragment(MenuItem item) {
+
+        item.setChecked(true);
+
+        switch (item.getItemId()) {
+            case R.id.action_face:
+                // Action to perform when Face Menu item is selected
+                pushFragment(new FacePageFragment());
+                break;
+            case R.id.action_resume:
+                // Action to perform when Resume Menu item is selected
+                pushFragment(new ResumePageFragment());
+                break;
+            case R.id.action_game:
+                // Action to perform when Game Menu item is selected
+                pushFragment(new GamePageFragment());
+                break;
+            case R.id.action_me:
+                // Action to perform when Me Menu item is selected
+                pushFragment(new MePageFragment());
+                break;
+        }
+
+    }
+
+    /**
+     * Method to push any fragment into given id.
+     * @param fragment An instance of Fragment to show into the given id.
+     */
+
+    protected void pushFragment(Fragment fragment) {
+        if (fragment == null)
+            return;
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager != null) {
+            FragmentTransaction ft = fragmentManager.beginTransaction();
+            if (ft != null) {
+                ft.replace(R.id.rootLayout, fragment);
+                ft.commit();
             }
-        });
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
