43b4c04ac732b5468b6b7cb644e751c99f76b9f6
BaiDingApp <30397356+BaiDingApp@users.noreply.github.com>
2017-07-25 23:50:19 +0800

    2017/07/25 My Information Page for Single has been completed

---
+++ app/src/main/java/com/baidingapp/faceapp/MyInformationSingleActivity.java
@@ -0,0 +1,13 @@
+package com.baidingapp.faceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyInformationSingleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_information_single);
+    }
+}
