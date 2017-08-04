06808199ba5757540b16bed7525d8e1e90d4e328
BaiDingApp <30397356+BaiDingApp@users.noreply.github.com>
2017-08-04 18:57:41 +0800

    2017/08/04 BasicInformationDefaultActivity can save data into LeanCloud.

--- app/src/main/java/com/baidingapp/faceapp/BasicInformationDefaultActivity.java
+++ app/src/main/java/com/baidingapp/faceapp/BasicInformationDefaultActivity.java
@@ -9,7 +9,23 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
-public class BasicInformationDefaultActivity extends AppCompatActivity {
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.AVUser;
+import com.avos.avoscloud.SaveCallback;
+
+public class BasicInformationDefaultActivity extends AppCompatActivity
+        implements AdapterView.OnItemSelectedListener {
+
+    private int mEducationPosition;
+    private int mOccupationPosition;
+    private int mBirthPlacePosition;
+    private int mWorkPlacePosition;
+    private int mOverseaPosition;
+    private int mSinglePosition;
+    private int mReligionPosition;
+    private int mPetPosition;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,28 +33,107 @@ public class BasicInformationDefaultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_basic_information_default);
 
 
+        // onClick Education spinner
+        Spinner mEducationSpinner = (Spinner) findViewById(R.id.action_basic_education_default);
+        ArrayAdapter mEducationAdapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_education_levels, android.R.layout.simple_spinner_item);
+        mEducationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mEducationSpinner.setAdapter(mEducationAdapter);
+        mEducationSpinner.setOnItemSelectedListener(this);
+
+
+        // onClick Occupation spinner
+        Spinner mOccupationSpinner = (Spinner) findViewById(R.id.action_basic_occupation_default);
+        ArrayAdapter mOccupationAdapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_occupations, android.R.layout.simple_spinner_item);
+        mOccupationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mOccupationSpinner.setAdapter(mOccupationAdapter);
+        mOccupationSpinner.setOnItemSelectedListener(this);
+
+
+        // onClick BirthPlace spinner
+        Spinner mBirthPlaceSpinner = (Spinner) findViewById(R.id.action_basic_birth_place_default);
+        ArrayAdapter mBirthPlaceAdapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_place, android.R.layout.simple_spinner_item);
+        mBirthPlaceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mBirthPlaceSpinner.setAdapter(mBirthPlaceAdapter);
+        mBirthPlaceSpinner.setOnItemSelectedListener(this);
+
+
+        // onClick WorkPlace spinner
+        Spinner mWorkPlaceSpinner = (Spinner) findViewById(R.id.action_basic_work_place_default);
+        ArrayAdapter mWorkPlaceAdapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_place, android.R.layout.simple_spinner_item);
+        mWorkPlaceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mWorkPlaceSpinner.setAdapter(mWorkPlaceAdapter);
+        mWorkPlaceSpinner.setOnItemSelectedListener(this);
+
+
+        // onClick Oversea spinner
+        Spinner mOverseaSpinner = (Spinner) findViewById(R.id.action_basic_oversea_default);
+        ArrayAdapter mOverseaAdapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_yes_no, android.R.layout.simple_spinner_item);
+        mOverseaAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mOverseaSpinner.setAdapter(mOverseaAdapter);
+        mOverseaSpinner.setOnItemSelectedListener(this);
+
+
+        // onClick Single spinner
         Spinner mSingleSpinner = (Spinner) findViewById(R.id.action_basic_single_default);
         ArrayAdapter mSingleAdapter = ArrayAdapter.createFromResource(this,
                 R.array.spinner_yes_no, android.R.layout.simple_spinner_item);
         mSingleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mSingleSpinner.setAdapter(mSingleAdapter);
-        mSingleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                MainActivity.mIsSingle = (position==1);
-            }
+        mSingleSpinner.setOnItemSelectedListener(this);
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
 
-            }
-        });
+        // onClick Religion spinner
+        Spinner mReligionSpinner = (Spinner) findViewById(R.id.action_basic_religion_default);
+        ArrayAdapter mReligionAdapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_yes_no, android.R.layout.simple_spinner_item);
+        mReligionAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mReligionSpinner.setAdapter(mReligionAdapter);
+        mReligionSpinner.setOnItemSelectedListener(this);
+
+
+        // onClick Pet spinner
+        Spinner mPetSpinner = (Spinner) findViewById(R.id.action_basic_pet_default);
+        ArrayAdapter mPetAdapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_yes_no, android.R.layout.simple_spinner_item);
+        mPetAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mPetSpinner.setAdapter(mPetAdapter);
+        mPetSpinner.setOnItemSelectedListener(this);
 
 
         Button mSubmitButton = (Button) findViewById(R.id.action_submit_basic_info_default);
         mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Save data into LeanCloud
+                AVObject mBasicInfo = new AVObject("BasicInfo");
+                mBasicInfo.put("username", AVUser.getCurrentUser().getUsername());
+                mBasicInfo.put("educationLevel", mEducationPosition);
+                mBasicInfo.put("occupation", mOccupationPosition);
+                mBasicInfo.put("birthPlace", mBirthPlacePosition);
+                mBasicInfo.put("workPlace", mWorkPlacePosition);
+                mBasicInfo.put("oversea", mOverseaPosition);
+                mBasicInfo.put("single", mSinglePosition);
+                mBasicInfo.put("religion", mReligionPosition);
+                mBasicInfo.put("pet", mPetPosition);
+                mBasicInfo.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(AVException e) {
+                        /*
+                        if (e == null) {
+                            // Successfully saved
+                        } else {
+                            // Fail to save
+                        }
+                        */
+                    }
+                });
+
+
                 // TODO
                 // If the former context is RegisterActivity, then finish the current context,
                 //     and start the MainActivity.
@@ -47,5 +142,49 @@ public class BasicInformationDefaultActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch(parent.getId()){
+            case R.id.action_basic_education_default:
+                mEducationPosition = position;
+                break;
+
+            case R.id.action_basic_occupation_default:
+                mOccupationPosition = position;
+                break;
+
+            case R.id.action_basic_birth_place_default:
+                mBirthPlacePosition = position;
+                break;
+
+            case R.id.action_basic_work_place_default:
+                mWorkPlacePosition = position;
+                break;
+
+            case R.id.action_basic_oversea_default:
+                mOverseaPosition = position;
+                break;
+
+            case R.id.action_basic_single_default:
+                mSinglePosition = position;
+                MainActivity.mIsSingle = (position==1);
+                break;
+
+            case R.id.action_basic_religion_default:
+                mReligionPosition = position;
+                break;
+
+            case R.id.action_basic_pet_default:
+                mPetPosition = position;
+                break;
+        }
+    }
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
