e51c38cb8a5ee2e05dba13e900c0d735bf4bba4a
BaiDingApp <30397356+BaiDingApp@users.noreply.github.com>
2017-08-05 23:31:01 +0800

    2017/08/05 Update the LeanCloud SDK

--- app/src/main/java/com/baidingapp/faceapp/OutputRateFaceActivity.java
+++ app/src/main/java/com/baidingapp/faceapp/OutputRateFaceActivity.java
@@ -1,19 +1,38 @@
 package com.baidingapp.faceapp;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVFile;
+import com.avos.avoscloud.ProgressCallback;
+import com.avos.avoscloud.SaveCallback;
 import com.baidingapp.faceapp.helper.ImageHelper;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class OutputRateFaceActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_IMAGE = 1;
+
+    private String mImageName;
+    private File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +42,41 @@ public class OutputRateFaceActivity extends AppCompatActivity {
 
         // Upload and show face image
         ImageView mFaceImageView = (ImageView) findViewById(R.id.action_upload_photo_output_rate);
+        mFaceImageView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent, REQUEST_CODE_IMAGE);
+
+
+                AVFile mImageFile = null;
+                try {
+                    mImageFile = AVFile.withFile(mImageName, file);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+
+                mImageFile.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(AVException e) {
+                        if (e == null) {
+                            Log.d("saved", "文件上传成功！");
+                        } else {
+                            Log.d("saved", "文件上传失败! " + e.getMessage() + "|" + e.getCause());
+                        }
+                    }
+                }, new ProgressCallback() {
+                    @Override
+                    public void done(Integer integer) {
+                        // 上传进度数据，integer 介于 0 和 100。
+                    }
+                });
+
+            }
+        });
 
         // The URL is used to test Glide
         String imageUrl = "http://www.fdsm.fudan.edu.cn/UserWebEditorUploadImage/upload/image/20160428/6359744927934022586120687.jpg";
@@ -34,6 +88,44 @@ public class OutputRateFaceActivity extends AppCompatActivity {
         showResult();
 
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode != Activity.RESULT_OK) {
+            return;
+        }
+
+        if(requestCode == REQUEST_CODE_IMAGE) {
+            if(data == null) {
+                return;
+            }
+
+            Uri imageUri = data.getData();
+            String[] imagePathSplit = imageUri.getPath().split(":");
+
+            String mImagePath;
+            if(imagePathSplit.length == 2) {
+                // High version
+                String relativeImagePath = imagePathSplit[1];
+                mImagePath = Environment.getExternalStorageDirectory() +"/" + relativeImagePath;
+            } else {
+                // Low version
+                mImagePath = imageUri.getPath();
+            }
+
+            try {
+                file = new File(new URI(mImagePath));
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+
+
+            String[] folders = mImagePath.split("/");
+            mImageName = folders[folders.length-1];
+        }
+    }
+
 
     private void showResult() {
         // Plot the rates by others
