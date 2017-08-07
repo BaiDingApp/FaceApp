959d2f54d510fa513173fdbb8ed2f3cc45f012d3
BaiDingApp <30397356+BaiDingApp@users.noreply.github.com>
2017-08-08 00:22:05 +0800

    2017/08/07 1. Reset up the Storage module of LeanCloud according to Gradle method, and delete the related 4 jars from AS, and compile the other 3 of them in build.gradle according to the official method. 2. Compile the Permission Dispatcher into to project for runtime permission. 3. Handle the potential conflict of Glide and  Permission Dispatcher into (META-INF/LICENSE/NOTICE). 4. The physical phone can write text and image data to LeanCloud. 5. The main is to find the real path of an image ... and from which the AVFile can be defined ...

--- app/src/main/java/com/baidingapp/faceapp/OutputRateFaceActivity.java
+++ app/src/main/java/com/baidingapp/faceapp/OutputRateFaceActivity.java
@@ -1,17 +1,23 @@
 package com.baidingapp.faceapp;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
-import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVFile;
+import com.avos.avoscloud.AVUser;
 import com.avos.avoscloud.ProgressCallback;
 import com.avos.avoscloud.SaveCallback;
 import com.baidingapp.faceapp.helper.ImageHelper;
@@ -20,19 +26,19 @@ import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
 public class OutputRateFaceActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_IMAGE = 1;
 
-    private String mImageName;
-    private File file;
+    private String mImagePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,42 +47,23 @@ public class OutputRateFaceActivity extends AppCompatActivity {
 
 
         // Upload and show face image
-        ImageView mFaceImageView = (ImageView) findViewById(R.id.action_upload_photo_output_rate);
+        ImageView mFaceImageView = (ImageView) findViewById(R.id.face_image_output_rate);
         mFaceImageView.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                startActivityForResult(intent, REQUEST_CODE_IMAGE);
-
-
-                AVFile mImageFile = null;
-                try {
-                    mImageFile = AVFile.withFile(mImageName, file);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-
-                mImageFile.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(AVException e) {
-                        if (e == null) {
-                            Log.d("saved", "文件上传成功！");
-                        } else {
-                            Log.d("saved", "文件上传失败! " + e.getMessage() + "|" + e.getCause());
-                        }
-                    }
-                }, new ProgressCallback() {
-                    @Override
-                    public void done(Integer integer) {
-                        // 上传进度数据，integer 介于 0 和 100。
-                    }
-                });
-
+                OutputRateFaceActivityPermissionsDispatcher.pickPhotoFromGalleryWithCheck(OutputRateFaceActivity.this);
             }
         });
+
+
+        Button mUploadButton = (Button) findViewById(R.id.action_upload_face_photo_button);
+        mUploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadPhotoToLeanCloud();
+            }
+        });
+
 
         // The URL is used to test Glide
         String imageUrl = "http://www.fdsm.fudan.edu.cn/UserWebEditorUploadImage/upload/image/20160428/6359744927934022586120687.jpg";
@@ -86,12 +73,12 @@ public class OutputRateFaceActivity extends AppCompatActivity {
 
         // Show the rate by others
         showResult();
-
     }
 
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
         if(resultCode != Activity.RESULT_OK) {
             return;
         }
@@ -101,29 +88,83 @@ public class OutputRateFaceActivity extends AppCompatActivity {
                 return;
             }
 
-            Uri imageUri = data.getData();
-            String[] imagePathSplit = imageUri.getPath().split(":");
+            mImagePath = getRealPathFromURI(OutputRateFaceActivity.this, data.getData());
 
-            String mImagePath;
-            if(imagePathSplit.length == 2) {
-                // High version
-                String relativeImagePath = imagePathSplit[1];
-                mImagePath = Environment.getExternalStorageDirectory() +"/" + relativeImagePath;
-            } else {
-                // Low version
-                mImagePath = imageUri.getPath();
-            }
-
-            try {
-                file = new File(new URI(mImagePath));
-            } catch (URISyntaxException e) {
-                e.printStackTrace();
-            }
-
-
-            String[] folders = mImagePath.split("/");
-            mImageName = folders[folders.length-1];
         }
+    }
+
+
+    // Get the real path of the file from its Uri
+    private String getRealPathFromURI(Context context, Uri contentUri) {
+        if (contentUri.getScheme().equals("file")) {
+            return contentUri.getPath();
+        } else {
+            Cursor cursor = null;
+            try {
+                String[] mDataString = {MediaStore.Images.Media.DATA};
+                cursor = context.getContentResolver().query(contentUri, mDataString, null, null, null);
+                if (null != cursor) {
+                    int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                    cursor.moveToFirst();
+                    return cursor.getString(column_index);
+                } else {
+                    return "";
+                }
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+        }
+    }
+
+
+    // Upload Photo to LeanCloud
+    @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+    void pickPhotoFromGallery() {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK, null);
+        photoPickerIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+        startActivityForResult(photoPickerIntent, REQUEST_CODE_IMAGE);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // NOTE: Delegate the permission handling to generated method
+        OutputRateFaceActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+
+    private void uploadPhotoToLeanCloud() {
+        AVFile mImageFile = null;
+        try {
+            mImageFile = AVFile.withAbsoluteLocalPath(AVUser.getCurrentUser().toString()+".jpg", mImagePath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        /*
+        AVObject photoInfo = new AVObject("PhotoInfo");
+        photoInfo.put("username", AVUser.getCurrentUser());
+        photoInfo.put("photo", mImageFile);
+        */
+
+        mImageFile.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(AVException e) {
+                if (e == null) {
+                    Log.d("saved", "文件上传成功！");
+                } else {
+                    Log.d("saved", "文件上传失败! " + e.getMessage() + "|" + e.getCause());
+                }
+            }
+        }, new ProgressCallback() {
+            @Override
+            public void done(Integer integer) {
+                // Uploading Progress Indicator，an integer between 0 and 100。
+            }
+        });
     }
 
 
